d4168e5ca5e3575626de714d40574d7737d8e606
huzi <git3.1415926@gmail.com>
2020-09-18 03:51:22 +0800

    这是我1块钱买的课程，哈哈

---
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -0,0 +1,40 @@
+package com.imooc.luckymoney;
+
+import org.jboss.logging.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.management.ValueExp;
+import java.math.BigDecimal;
+
+/**
+ * @Program: luckymoney1
+ * @ClassName: HelloController
+ * @Description: TODD
+ * @Author: Administrator
+ * @Date: 2020/9/16
+ * @Controller + @ResponseBody = @RestController
+ **/
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    private LimitConfig limitConfig;
+
+    //1、@GetMapping("/say/{id}") + @PathVariable("id") Integer age        -----http://localhost:8081/luckymoney/hello/say/320
+    //2、@GetMapping("/say") + @RequestParam(value = "id",required = false,defaultValue = "0") Integer age        -----http://localhost:8081/luckymoney/hello/say
+    //关于get请求方式，1跟2的方式都可以
+    //@PostMapping("/say")
+    //3、@GetMapping("/say") + @PostMapping("/say") = @RequestMapping("/say")
+    @GetMapping("/say")
+//    @RequestMapping("/say")
+    public String say(@RequestParam(value = "id",required = false,defaultValue = "0") Integer age) {
+//        System.out.println("跟慕课网廖师兄学springboot。");
+//        return "跟慕课网廖师兄学springboot。";
+//        return "说明：" + limitConfig.getDescription();
+//        return "index";
+        return "id = " + age;
+    }
+}
---
+++ src/main/java/com/imooc/luckymoney/LuckymoneyService.java
@@ -0,0 +1,37 @@
+package com.imooc.luckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+/**
+ * @Program: luckymoney
+ * @ClassName: LuckymoneyService
+ * @Description: TODD
+ * @Author: Administrator
+ * @Date: 2020/9/18
+ **/
+@Service
+public class LuckymoneyService {
+    @Autowired
+    private LuckymoneyRepository repository;
+
+    /**
+     * 事务，指数据库的事务，要么都执行，要么都不执行
+     * 例如：扣库存 > 创建订单
+     */
+    @Transactional
+    public void createTwo(){
+        Luckymoney luckymoney1 = new Luckymoney();
+        luckymoney1.setProducer("刘成虎");
+        luckymoney1.setMoney(new BigDecimal("520"));
+        repository.save(luckymoney1);
+        Luckymoney luckymoney2 = new Luckymoney();
+        luckymoney2.setProducer("刘成虎");
+        luckymoney2.setMoney(new BigDecimal("1314"));
+        repository.save(luckymoney2);
+
+    }
+}
